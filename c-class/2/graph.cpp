ead3332a53c49f4a27d3d645ca6114c26e5240c2
misty-winding <misty.winding@gmail.com>
2013-11-04 02:13:32 +0900

    finish Graph class

--- c-class/2/graph.cpp
+++ c-class/2/graph.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <iostream>
 #include <iomanip>
+#include <cstdlib>
+#include <ctime>
 
 
 using std::vector;
@@ -24,68 +26,252 @@ typedef struct {
 
 class Graph {
 public:
-    Graph(int vertices, double density, double distance_min, double distance_max)
-        : vertices_(vertices),
-          density_(density),
-          distance_min_(distance_min),
-          distance_max_(distance_max)
-    {
-        assert(0 != vertices_);
+    Graph(int vertices, double density, double distance_min, double distance_max);
+    ~Graph() { delete p_matrix_; }
 
-        int element_num = vertices_ * vertices_;
-        p_matrix_ = new vector<ST_EDGE>(element_num);
-
-        for (int i = 0; i < element_num; i++) {
-            (*p_matrix_)[i].b_connect = false;
-            (*p_matrix_)[i].distance = 10.3;
-        }
-    }
-
-    ~Graph()
-    {
-        delete p_matrix_;
-    }
-
-    int get_number_of_vertices(void) { return vertices_; }
-
-    bool adjance(int row, int col)
-    {
-        ST_EDGE* p_edge;
-        ST_POINT point;
-
-        point.row = row;
-        point.col = col;
-        p_edge = get_edge(point);
-    }
+    int GetNumberOfVertices(void) { return vertices_; }
+    int GetNumberOfEdges(void) { return edges_; }
+    bool Adjance(int row, int col);
+    int Neighbors(int vertex_index, int* p_neighbor_list);
+    void Add(int vertex_index_x, int vertex_index_y, double distance);
 
     friend ostream& operator<< (ostream& out, Graph& graph);
 
 
 private:
     int vertices_;
+    int edges_;
     double density_;
     double distance_min_;
     double distance_max_;
 
     vector<ST_EDGE>* p_matrix_;
 
-    inline int get_index_of_matrix(const ST_POINT &point)
+    inline int get_index_in_matrix(const ST_POINT &point)
     {
+        assert(point.row < vertices_);
+        assert(point.col < vertices_);
+
         return point.row * vertices_ + point.col;
     }
 
-    inline void get_row_col_of_matrix(int index, ST_POINT &point)
+    inline int get_index_in_matrix(int vertex_index)
     {
+        assert(vertex_index < vertices_);
+
+        return vertex_index * vertices_;
+    }
+
+    inline void get_point_in_matrix(int index, ST_POINT &point)
+    {
+        assert(index < vertices_ * vertices_);
+
         point.row = index / vertices_;
         point.col = index % vertices_;
     }
 
-    inline ST_EDGE* get_edge(const ST_POINT &point)
+    inline ST_EDGE* get_edge_info(int index)
     {
-        int index = get_index_of_matrix(point);
+        assert(index < vertices_ * vertices_);
+
         return &(*p_matrix_)[index];
     }
+
+    inline ST_EDGE* get_edge_info(const ST_POINT &point)
+    {
+        int index = get_index_in_matrix(point);
+        return &(*p_matrix_)[index];
+    }
+
+    inline ST_EDGE* get_row_first_edge_info(int vertex_index)
+    {
+        int index = get_index_in_matrix(vertex_index);
+        ST_EDGE* p_edge = get_edge_info(index);
+
+        return p_edge;
+    }
+
+    void set_edge_info(const ST_POINT &point, const ST_EDGE edge);
+    void create_edge(void);
 };
+
+/*
+ ***************************************************************************************************
+ *   @brief
+ **************************************************************************************************/
+void Graph::set_edge_info(const ST_POINT &point, const ST_EDGE edge)
+{
+    ST_EDGE* p_edge;
+
+    if (point.row == point.col) {
+        return;
+    }
+
+    p_edge = get_edge_info(point);
+    *p_edge = edge;
+}
+
+/*
+ ***************************************************************************************************
+ *   @brief
+ **************************************************************************************************/
+double get_random_val(double max, double min)
+{
+    double random_val;
+    double range = max - min;
+
+    assert(min < max);
+
+    random_val = ((double)random() / ((double)RAND_MAX + 1)) * range + min;
+    return random_val;
+}
+
+/*
+ ***************************************************************************************************
+ *   @brief
+ **************************************************************************************************/
+#if 1
+void Graph::create_edge(void)
+{
+    double  random_val;
+    double  distance;
+
+    for (int row = 0; row < vertices_; row++) {
+        for (int col = 0; col < vertices_; col++) {
+            if (row <= col) {
+                continue;
+            }
+
+            random_val = get_random_val(1.0, 0.0);
+            if (random_val < density_) {
+                distance = get_random_val(distance_max_, distance_min_);
+                Add(col, row, distance);
+            }
+        }
+    }
+}
+
+#else
+
+/*
+ ***************************************************************************************************
+ *   @brief
+ **************************************************************************************************/
+void Graph::create_edge(void)
+{
+    ST_POINT point;
+    double  distance;
+    bool b_connect;
+    int edge_num = (vertices_ * (vertices_ - 1) / 2) * density_;
+    int random_value;
+    int index_max_in_matrix = vertices_ * vertices_ - 1;
+
+    do {
+        random_value = get_random_val(index_max_in_matrix, 0);
+        get_point_in_matrix(random_value, point);
+        b_connect = Adjance(point.row, point.col);
+        if (!b_connect) {
+            distance = get_random_val(distance_max_, distance_min_);
+            Add(point.col, point.row, distance);
+            edge_num--;
+        }
+    } while (0 < edge_num);
+}
+
+#endif
+
+
+
+// interface
+
+
+
+/*
+ ***************************************************************************************************
+ *   @brief
+ **************************************************************************************************/
+Graph::Graph(int vertices, double density, double distance_min, double distance_max)
+        : vertices_(vertices),
+          density_(density),
+          distance_min_(distance_min),
+          distance_max_(distance_max)
+{
+    assert(0 != vertices_);
+
+    ST_EDGE edge;
+
+    int element_num = vertices_ * vertices_;
+    p_matrix_ = new vector<ST_EDGE>(element_num);
+
+    edge.b_connect = false;
+    edge.distance = 0.0;
+
+    for (int i = 0; i < element_num; i++) {
+        (*p_matrix_)[i] = edge;
+    }
+
+    edges_ = 0;
+    create_edge();
+}
+
+/*
+ ***************************************************************************************************
+ *   @brief
+ **************************************************************************************************/
+bool Graph::Adjance(int row, int col)
+{
+    ST_EDGE* p_edge;
+    ST_POINT point;
+
+    point.row = row;
+    point.col = col;
+    p_edge = get_edge_info(point);
+
+    return p_edge->b_connect;
+}
+
+/*
+ ***************************************************************************************************
+ *   @brief
+ **************************************************************************************************/
+int Graph::Neighbors(int vertex_index, int* p_neighbor_list)
+{
+    ST_EDGE* p_edge = get_row_first_edge_info(vertex_index);
+    int node_num = 0;
+
+    for (int i = 0; i < vertices_; i++) {
+        if (p_edge->b_connect) {
+            *p_neighbor_list = i;
+            p_neighbor_list++;
+            node_num++;
+        }
+    }
+
+    return node_num;
+}
+
+/*
+ ***************************************************************************************************
+ *   @brief
+ **************************************************************************************************/
+void Graph::Add(int vertex_index_x, int vertex_index_y, double distance)
+{
+    ST_POINT point;
+    ST_EDGE  new_edge;
+
+    new_edge.b_connect = true;
+    new_edge.distance = distance;
+
+    point.row = vertex_index_x;
+    point.col = vertex_index_y;
+    set_edge_info(point, new_edge);
+
+    point.row = vertex_index_y;
+    point.col = vertex_index_x;
+    set_edge_info(point, new_edge);
+
+    edges_++;
+}
 
 /*
  ***************************************************************************************************
@@ -99,7 +285,7 @@ ostream& operator<< (ostream& out, Graph& graph)
 
     // column number
     //
-    cout << "   ";
+    cout << "r/c";
     for (int col = 0; col < graph.vertices_; col++) {
         cout << std::setw(disp_width);
         cout << col;
@@ -127,7 +313,7 @@ ostream& operator<< (ostream& out, Graph& graph)
         for (int col = 0; col < graph.vertices_; col++) {
             point.row = row;
             point.col = col;
-            p_edge = graph.get_edge(point);
+            p_edge = graph.get_edge_info(point);
 
             cout << std::setw(disp_width);
             if (p_edge->b_connect) {
@@ -143,15 +329,31 @@ ostream& operator<< (ostream& out, Graph& graph)
     return out;
 }
 
+/*
+ ***************************************************************************************************
+ *   @brief
+ **************************************************************************************************/
 void TestGraph(void)
 {
+    srandom((unsigned)time(NULL));
+
     int vertices = 10;
     double density = 0.1;
     double distance_min = 1.0;
     double distance_max = 10.0;
 
-    Graph graph(vertices, density, distance_min, distance_max);
-    cout << graph;
+    double edges = 0.0;
+    int try_num = 2;
+
+    for (int i = 0; i < try_num; i++) {
+        Graph graph(vertices, density, distance_min, distance_max);
+        edges += graph.GetNumberOfEdges();
+
+        cout << graph;
+    }
+
+    double edges_average = edges / try_num;
+    cout << "edges_average = " << edges_average << endl;
 }
 
 // EOF
